af3d7a775eadcb73426b7c8b31a2d71eee490a08
kaisomir <kaisomir@gmail.com>
2023-06-11 11:49:34 +0200

    Initial commit

---
+++ src/main/java/io/github/kaisomir/SpigotKiss.java
@@ -0,0 +1,35 @@
+package io.github.kaisomir;
+
+import org.bukkit.Bukkit;
+import org.bukkit.Particle;
+import org.bukkit.entity.Entity;
+import org.bukkit.entity.EntityType;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerInteractEntityEvent;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class SpigotKiss extends JavaPlugin implements Listener {
+
+    @Override
+    public void onEnable() {
+        Bukkit.getPluginManager().registerEvents(this, this);
+        // getLogger().info("onEnable is called!");
+    }
+    @Override
+    public void onDisable() {
+        // getLogger().info("onDisable is called!");
+    }
+
+    @EventHandler
+    public void PlayerInteractEntityEvent(PlayerInteractEntityEvent event)
+    {
+        Player p = event.getPlayer();
+        Entity interacted = event.getRightClicked();
+        if (interacted.getType().equals(EntityType.PLAYER) && p.isSneaking())
+        {
+            p.getWorld().spawnParticle(Particle.HEART, interacted.getLocation().add(0,1.8,0),1);
+        }
+    }
+}
